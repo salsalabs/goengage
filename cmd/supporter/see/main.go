b67361ae7ada2b0f5e511a2030bee12a5d29f131
Allen Leonard <aleonard@salsalabs.com>
2019-06-12 12:33:10 -0500

    Add validation for Required() tags in kingpin.

--- cmd/supporter/see/main.go
+++ cmd/supporter/see/main.go
@@ -17,6 +17,14 @@ func main() {
 		email = app.Flag("email", "Email address to look up").Required().String()
 	)
 	app.Parse(os.Args[1:])
+	if login == nil || len(*login) == 0 {
+		fmt.Println("Error --login is required.")
+		os.Exit(1)
+	}
+	if email == nil || len(*email) == 0 {
+		fmt.Println("Error --email is required.")
+		os.Exit(1)
+	}
 	e, err := goengage.Credentials(*login)
 	if err != nil {
 		panic(err)
