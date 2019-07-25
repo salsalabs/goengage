7a339b042e037f163d43356d633a77cf6e4ed53e
Allen Leonard <aleonard@salsalabs.com>
2019-07-25 17:07:14 -0500

    Added AllSegments.

--- pkg/segment.go
+++ pkg/segment.go
@@ -1,7 +1,15 @@
 package goengage
 
+import (
+	"fmt"
+	"net/http"
+)
+
 //SegSearch is used to search for segments.
 const SegSearch = "/api/integration/ext/v1/segments/search"
+
+//SegSupporterSearch is used to search segments for supporters.
+const SegSupporterSearch = "/api/integration/ext/v1/segments/members/search"
 
 //Segment is a named group of supporters.
 type Segment struct {
@@ -31,4 +39,57 @@ type SegSearchResult struct {
 		Total    int32     `json:"total,omitempty"`
 		Segments []Segment `json:"segments,omitempty"`
 	} `json:"payload,omitempty"`
+}
+
+//SegSupporterSearchRequest is used to find supporters in a segment.
+//Be sure to pass the correct IdentifierType.
+//SegSearchRequest is used to ask for supporters.
+type SegSupporterSearchRequest struct {
+	SegmentID    string   `json:"segmentId"`
+	SupporterIDs []string `json:"supporterIds"`
+	Offset       int32    `json:"offset,omitempty"`
+	Count        int32    `json:"count,omitempty"`
+}
+
+//SegSupporterSearchResult is returned when supporters are found by a search.
+type SegSupporterSearchResult struct {
+	Payload struct {
+		Count      int32       `json:"count,omitempty"`
+		Offset     int32       `json:"offset,omitempty"`
+		Total      int32       `json:"total,omitempty"`
+		Supporters []Supporter `json:"supporters,omitempty"`
+	} `json:"payload,omitempty"`
+}
+
+//AllSegments returns all groups.
+func AllSegments(e *EngEnv, m *MetricData, c bool) ([]Segment, error) {
+	rqt := SegSearchRequest{
+		Offset:       0,
+		Count:        m.MaxBatchSize,
+		MemberCounts: c,
+	}
+	var resp SegSearchResult
+	n := NetOp{
+		Host:     e.Host,
+		Fragment: SegSearch,
+		Method:   http.MethodPost,
+		Token:    e.Token,
+		Request:  &rqt,
+		Response: &resp,
+	}
+	var a []Segment
+	for rqt.Count > 0 {
+		fmt.Printf("Reading %d from %d\n", rqt.Count, rqt.Offset)
+		err := n.Do()
+		if err != nil {
+			panic(err)
+		}
+		for _, s := range resp.Payload.Segments {
+			a = append(a, s)
+		}
+		count := len(resp.Payload.Segments)
+		rqt.Count = int32(count)
+		rqt.Offset = rqt.Offset + int32(count)
+	}
+	return a, nil
 }
