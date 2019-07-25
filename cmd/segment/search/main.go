ea74f8b2164fc20a3653b35dd865156a948d4d58
Allen Leonard <aleonard@salsalabs.com>
2019-07-25 17:07:44 -0500

    Implement using AllSegments.

--- cmd/segment/search/main.go
+++ cmd/segment/search/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/salsalabs/goengage/pkg"
@@ -24,19 +23,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	rqt := goengage.SegSearchRequest{
-		Offset:       0,
-		Count:        m.MaxBatchSize,
-		MemberCounts: !*fast,
-	}
-	var resp goengage.SegSearchResult
-	n := goengage.NetOp{
-		Host:     e.Host,
-		Fragment: goengage.SegSearch,
-		Method:   http.MethodPost,
-		Token:    e.Token,
-		Request:  &rqt,
-		Response: &resp,
+	a, err := goengage.AllSegments(e, m, !*fast)
+	if err != nil {
+		panic(err)
 	}
 	dashes := "----------------------------------------------------------"
 	fh := "%-36v %-40v %-10v %7v %-8v %v\n"
@@ -44,29 +33,18 @@ func main() {
 	fmt.Println()
 	fmt.Printf(fh, "SegmentID", "Name", "Type", "Members", "ExtID", "Description")
 	fmt.Printf(fh, dashes[0:36], dashes[0:40], dashes[0:10], dashes[0:7], dashes[0:8], dashes[0:25])
-
-	for rqt.Count > 0 {
-		fmt.Printf("Reading %d from %d\n", rqt.Count, rqt.Offset)
-		err = n.Do()
-		if err != nil {
-			panic(err)
+	for _, s := range a {
+		name := s.Name
+		if len(name) > 40 {
+			name = name[0:37] + "..."
 		}
-		for _, s := range resp.Payload.Segments {
-			name := s.Name
-			if len(name) > 40 {
-				name = name[0:37] + "..."
-			}
-			fmt.Printf(fl,
-				s.SegmentID,
-				name,
-				s.Type,
-				s.TotalMembers,
-				s.ExternalSystemID,
-				s.Description)
-		}
-		count := len(resp.Payload.Segments)
-		rqt.Count = int32(count)
-		rqt.Offset = rqt.Offset + int32(count)
+		fmt.Printf(fl,
+			s.SegmentID,
+			name,
+			s.Type,
+			s.TotalMembers,
+			s.ExternalSystemID,
+			s.Description)
 	}
 	fmt.Println()
 }
