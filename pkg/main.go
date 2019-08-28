07e155d9eefc216294de56aa7c32629b284907b2
Allen Leonard <aleonard@salsalabs.com>
2019-08-28 16:10:16 -0500

    Added generic header to replace specific header.

--- pkg/main.go
+++ pkg/main.go
@@ -31,34 +31,26 @@ type Error struct {
 	FieldName string `json:"fieldName,omitempty"`
 }
 
-//RequestHeader contains an optional refID.
-type RequestHeader struct {
-	RefID string `json:"refId,omitempty"`
-}
-
 //Request is the common structure for a request.  YOur request object
 //gets stored in Payload automatically by net.Do().
 type Request struct {
-	Header  RequestHeader `json:"header,omitempty"`
-	Payload interface{}   `json:"payload"`
-}
-
-//ResponseHeader is the common object returned by calls to Engage.
-//Payloads are defined by the objects receiving the data, since they
-//need the payload.
-type ResponseHeader struct {
-	ProcessingTime int    `json:"processingTime,omitempty"`
-	ServerID       string `json:"serverID,omitempty"`
+	Header struct {
+		RefID string `json:"refId,omitempty"`
+	} `json:"header,omitempty"`
+	Payload interface{} `json:"payload"`
 }
 
 //Response is the common structure for a request.  YOur request object
 //gets stored in Payload automatically by net.Do().
 type Response struct {
-	ID        string         `json:"id,omitempty"`
-	Timestamp string         `json:"timestamp"`
-	Header    ResponseHeader `json:"header,omitempty"`
-	Errors    []Error        `json:"errors,omitempty"`
-	Payload   interface{}    `json:"payload,omitempty"`
+	ID        string `json:"id"`
+	Timestamp string `json:"timestamp"`
+	Header    struct {
+		ProcessingTime int    `json:"processingTime"`
+		ServerID       string `json:"serverId"`
+	} `json:"header,omitempty"`
+	Errors  []Error     `json:"errors,omitempty"`
+	Payload interface{} `json:"payload"`
 }
 
 //NewEnvironment creates a new Environment and initializes the metrics.
