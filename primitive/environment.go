f3941afb2016e91b6e5d4f070beb5290c11a6c43
Allen Leonard <aleonard@salsalabs.com>
2019-11-03 10:28:45 -0600

    Copied from original because lazy.

---
+++ primitive/environment.go
@@ -0,0 +1,87 @@
+package goengage
+
+import (
+	"net/http"
+)
+
+const (
+	//UatHost is the hostname for Engage instances on the test server.
+	UatHost = "hq.uat.igniteaction.net"
+	//ProdHost is the hostname for Engage instances on the production server.
+	ProdHost = "api.salsalabs.org"
+	//ContentType is always Javascript.
+	ContentType = "application/json"
+	//SearchMethod is always "POST" in Engage.
+	SearchMethod = http.MethodPost
+)
+
+//Environment is the Engage environment.
+type Environment struct {
+	Host    string
+	Token   string
+	Metrics MetricData
+}
+
+//Error is used to report Engage errors.
+type Error struct {
+	ID        string `json:"id,omitempty"`
+	Code      int    `json:"code,omitempty"`
+	Message   string `json:"message,omitempty"`
+	Details   string `json:"details,omitempty"`
+	FieldName string `json:"fieldName,omitempty"`
+}
+
+//Request is the common structure for a request.  YOur request object
+//gets stored in Payload automatically by net.Do().
+type Request struct {
+	Header struct {
+		RefID string `json:"refId,omitempty"`
+	} `json:"header,omitempty"`
+	Payload interface{} `json:"payload"`
+}
+
+//Response is the common structure for a request.  YOur request object
+//gets stored in Payload automatically by net.Do().
+type Response struct {
+	ID        string `json:"id"`
+	Timestamp string `json:"timestamp"`
+	Header    struct {
+		ProcessingTime int    `json:"processingTime"`
+		ServerID       string `json:"serverId"`
+	} `json:"header,omitempty"`
+	Errors  []Error     `json:"errors,omitempty"`
+	Payload interface{} `json:"payload"`
+}
+
+//NewEnvironment creates a new Environment and initializes the metrics.
+//Panics if updating the metrics returns an error.
+func NewEnvironment(h string, t string) Environment {
+	e := Environment{
+		Host:  h,
+		Token: t,
+	}
+	err := (&e).UpdateMetrics()
+	if err != nil {
+		panic(err)
+	}
+	return e
+}
+
+//UpdateMetrics reads metrics and returns them.
+func (e *Environment) UpdateMetrics() error {
+	var resp MetricData
+	n := NetOp{
+		Host:     e.Host,
+		Endpoint: MetricsCommand,
+		Method:   http.MethodGet,
+		Token:    e.Token,
+		Request:  nil,
+		Response: &resp,
+	}
+	err := n.Do()
+	if err != nil {
+		return err
+	}
+	e.Metrics = resp
+	return nil
+}
