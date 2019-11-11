37b6cb5842d40cc62fdd7636d14f94ebe54414c5
Allen Leonard <aleonard@salsalabs.com>
2019-11-11 13:34:51 -0600

    Accept comma-separated list of emails.

--- cmd/supporter/search_by_email/main.go
+++ cmd/supporter/search_by_email/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/salsalabs/goengage/pkg"
 	supporter "github.com/salsalabs/goengage/pkg/supporter"
@@ -15,7 +15,7 @@ func main() {
 	var (
 		app   = kingpin.New("see-supporter", "A command-line app to to show supporters for an email.")
 		login = app.Flag("login", "YAML file with API token").Required().String()
-		email = app.Flag("email", "Email address to look up").Required().String()
+		email = app.Flag("email", "Comma-separated list of email addresses to look up").Required().String()
 	)
 	app.Parse(os.Args[1:])
 	if login == nil || len(*login) == 0 {
@@ -35,15 +35,16 @@ func main() {
 	offset := int32(0)
 	// from := goengage.Date("2000-01-01T00:00:00.000Z")
 	// to := goengage.Date("2100-01-01T00:00:00.000Z")
+	emails := strings.Split(*email, ",")
 	for count == int32(e.Metrics.MaxBatchSize) {
 		payload := supporter.SupporterSearchPayload{
-			Identifiers:    []string{*email},
+			Identifiers:    emails,
 			IdentifierType: supporter.EmailAddressType,
 			Offset:         offset,
 			Count:          e.Metrics.MaxBatchSize,
 		}
 		rqt := supporter.SupporterSearch{
-			Header:  goengage.Header{},
+			Header:  goengage.RequestHeader{},
 			Payload: payload,
 		}
 		var resp supporter.SupporterSearchResults
@@ -59,19 +60,20 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		b, _ := json.MarshalIndent(rqt, "", "    ")
-		fmt.Printf("Request:\n%v\n", string(b))
-		b, _ = json.MarshalIndent(resp, "", "    ")
-		fmt.Printf("Response:\n%v\n", string(b))
-
 		count = resp.Payload.Count
 		for i, s := range resp.Payload.Supporters {
 			email := goengage.FirstEmail(s)
-			fmt.Printf("%2d %-20v %-20v %v\n",
+			e := ""
+			if email != nil {
+				e = *email
+			}
+			fmt.Printf("%2d %-20v %-20v %-25v %-25v %v\n",
 				i+1,
 				s.FirstName,
 				s.LastName,
-				email)
+				s.CreatedDate,
+				s.LastModified,
+				e)
 		}
 	}
 }
