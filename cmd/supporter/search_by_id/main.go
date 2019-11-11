0e12f8126fd335b7c1854ca89ba47bde3e1e64f4
Allen Leonard <aleonard@salsalabs.com>
2019-11-11 14:47:10 -0600

    Just look up by ID from a CSV.

--- cmd/supporter/search_by_id/main.go
+++ cmd/supporter/search_by_id/main.go
@@ -3,31 +3,13 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"math"
 	"os"
+	"sort"
 
-	"github.com/salsalabs/goengage/pkg"
+	goengage "github.com/salsalabs/goengage/pkg"
+	supporter "github.com/salsalabs/goengage/pkg/supporter"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
-
-type Input struct {
-	InternalID     string
-	ExternalID     string
-	Email          string
-	CorrectedEmail string
-}
-
-func NewInput(a []string) Input {
-	i := Input{
-		InternalID:     a[0],
-		ExternalID:     a[1],
-		Email:          a[2],
-		CorrectedEmail: a[3],
-	}
-	return i
-}
-
-type InputMap map[string]Input
 
 func main() {
 	var (
@@ -49,68 +31,54 @@ func main() {
 	//records is an array of records.  Each record is
 	//an array of strings with these offsets.
 	//0 InternalID
-	//1 ExternalID
-	//2 Email
-	//3 Corrected Email
 	a, err := r.ReadAll()
+	if err != nil {
+		panic(err)
+	}
 	_ = f.Close()
 
-	var records []Input
-	inputMap := make(InputMap)
+	var lines []string
 	for _, r := range a {
 		if r[0] != "InternalID" {
-			i := NewInput(r)
-			inputMap[i.Email] = i
-			records = append(records, i)
-		}
-	}
-	rqt := goengage.SupSearchIDRequest{
-		IdentifierType: "EXTERNAL_ID",
-		Offset:         0,
-		Count:          e.Metrics.MaxBatchSize,
-	}
-	var resp goengage.SupSearchResult
-	n := goengage.NetOp{
-		Host:     e.Host,
-		Endpoint: goengage.SupSearch,
-		Method:   goengage.SearchMethod,
-		Token:    e.Token,
-		Request:  &rqt,
-		Response: &resp,
-	}
-
-	for rqt.Count > 0 {
-		rOffset := int32(rqt.Offset)
-		var identifiers []string
-		for i := int32(0); i < e.Metrics.MaxBatchSize && rOffset+i < int32(len(records)); i++ {
-			x := rOffset + i
-			identifiers = append(identifiers, records[x].ExternalID)
-		}
-		rqt.Count = int32(math.Min(float64(len(identifiers)), float64(e.Metrics.MaxBatchSize)))
-		if rqt.Count != 0 {
-			rqt.Identifiers = identifiers
-			fmt.Printf("Searching from offset %d for %d IDs\n", rqt.Offset, len(rqt.Identifiers))
-			err := n.Do()
+			searchPayload := supporter.SupporterSearchPayload{
+				IdentifierType: supporter.SupporterIDType,
+				Identifiers:    []string{r[0]},
+			}
+			rqt := supporter.SupporterSearch{
+				Header:  goengage.RequestHeader{},
+				Payload: searchPayload,
+			}
+			var resp supporter.SupporterSearchResults
+			n := goengage.NetOp{
+				Host:     e.Host,
+				Endpoint: supporter.Search,
+				Method:   goengage.SearchMethod,
+				Token:    e.Token,
+				Request:  &rqt,
+				Response: &resp,
+			}
+			err = n.Do()
 			if err != nil {
 				panic(err)
 			}
 
-			count := int32(len(resp.Payload.Supporters))
-			fmt.Printf("Read %d supporters from offset %d\n", count, rqt.Offset)
 			for _, s := range resp.Payload.Supporters {
-				e := goengage.FirstEmail(s)
-				email := ""
-				if e != nil && len(*e) > 0 {
-					email = *e
+				x := goengage.FirstEmail(s)
+				e := "(None)"
+				if x != nil {
+					e = *x
 				}
-				m, ok := inputMap[email]
-				if !ok {
-					fmt.Printf("Warning: unable to find email %v in the map\n", e)
-				} else {
-					fmt.Printf("%-20s %-30s -> %s\n", s.ExternalSystemID, email, m.CorrectedEmail)
-				}
+				name := fmt.Sprintf("%s %s", s.FirstName, s.LastName)
+				t := fmt.Sprintf("%-20s %-30s %-36v\n",
+					name,
+					e,
+					s.CreatedDate)
+				lines = append(lines, t)
 			}
 		}
-		rqt.Offset = rqt.Offset + rqt.Count
+	}
+	sort.Strings(lines)
+	for _, t := range lines {
+		fmt.Printf(t)
 	}
 }
