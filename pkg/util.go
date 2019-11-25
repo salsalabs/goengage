bc564ac30e9641a643df0d6049195196cdf4f427
Allen Leonard <aleonard@salsalabs.com>
2019-11-25 16:42:43 -0600

    Fix return value type.

--- pkg/util.go
+++ pkg/util.go
@@ -27,15 +27,15 @@ func FirstEmail(s Supporter) *string {
 
 //Date parses an Engage date and returns a Go time.
 func Date(s string) (t *time.Time) {
-	if len(p) == 0 {
+	if len(s) == 0 {
 		return t
 	}
 	p := strings.Replace(time.RFC3339Nano, "9999999Z07:00", "Z", -1)
-	t, err := time.Parse(p, s)
+	x, err := time.Parse(p, s)
 	if err != nil {
 		panic(err)
 	}
-	return &t
+	return &x
 }
 
 //UtilLogger is an environment to support a file logger.  It contains
