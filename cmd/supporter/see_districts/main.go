b4044b5ae3e295c2de1e1e5cd7bd772473e8110a
Allen Leonard <aleonard@salsalabs.com>
2020-02-20 16:22:23 -0600

    Converted to use a CSV with the email in the first column.

--- cmd/supporter/see_districts/main.go
+++ cmd/supporter/see_districts/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	goengage "github.com/salsalabs/goengage/pkg"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
@@ -14,16 +13,16 @@ import (
 //Program entry point.  Look for supporters with an email.  Errors are noisy and fatal.
 func main() {
 	var (
-		app   = kingpin.New("see_distrincts", "A command-line app to to show districs for supporter for email address(es).")
-		login = app.Flag("login", "YAML file with API token").Required().String()
-		email = app.Flag("email", "Comma-separated list of email addresses to look up").Required().String()
+		app     = kingpin.New("see_distrincts", "A command-line app to to show districs for supporter for email address(es).")
+		login   = app.Flag("login", "YAML file with API token").Required().String()
+		csvFile = app.Flag("csv", "Comma-separated file of email addresses to look up.  Email must be first.").Required().String()
 	)
 	app.Parse(os.Args[1:])
 	if login == nil || len(*login) == 0 {
 		fmt.Println("Error --login is required.")
 		os.Exit(1)
 	}
-	if email == nil || len(*email) == 0 {
+	if csvFile == nil || len(*csvFile) == 0 {
 		fmt.Println("Error --email is required.")
 		os.Exit(1)
 	}
@@ -56,84 +55,103 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// from := goengage.Date("2000-01-01T00:00:00.000Z")
-	// to := goengage.Date("2100-01-01T00:00:00.000Z")
-	emails := strings.Split(*email, ",")
-
-	payload := goengage.SupporterSearchPayload{
-		Identifiers:    emails,
-		IdentifierType: goengage.EmailAddressType,
-		Offset:         0,
-		Count:          e.Metrics.MaxBatchSize,
-	}
-	rqt := goengage.SupporterSearch{
-		Header:  goengage.RequestHeader{},
-		Payload: payload,
-	}
-	var resp goengage.SupporterSearchResults
-
-	n := goengage.NetOp{
-		Host:     e.Host,
-		Method:   goengage.SearchMethod,
-		Endpoint: goengage.SearchSupporter,
-		Token:    e.Token,
-		Request:  &rqt,
-		Response: &resp,
-	}
-	err = n.Do()
+	g, err := os.Open(*csvFile)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	for _, s := range resp.Payload.Supporters {
-		email := goengage.FirstEmail(s)
-		if email != nil {
-			fmt.Printf("%-36s %-25v %v\n", s.SupporterID, *email, s.Result)
-		} else { 
-			fmt.Printf("%-36s %-25v %v\n", s.SupporterID, "(None)", s.Result)
-		}
-		if s.Result != goengage.Found {
-			fmt.Printf("%v %v %v\n",
-				s.FirstName,
-				s.LastName,
-				s.Result)
-			continue
-		}
-		var r []string
-		e := ""
-		if email != nil {
-			e = *email
-		}
-		for i := 0; i < len(headers); i++ {
-			switch i {
-			case 0:
-				r = append(r, s.FirstName)
-			case 1:
-				r = append(r, s.LastName)
-			case 2:
-				r = append(r, e)
-			case 3:
-				r = append(r, s.Result)
-			case 4:
-				r = append(r, s.Address.AddressLine1)
-			case 5:
-				r = append(r, s.Address.AddressLine2)
-			case 6:
-				r = append(r, s.Address.City)
-			case 7:
-				r = append(r, s.Address.State)
-			case 8:
-				r = append(r, s.Address.PostalCode)
-			case 9:
-				r = append(r, s.Address.FederalHouseDistrict)
-			case 10:
-				r = append(r, s.Address.StateHouseDistrict)
-			case 11:
-				r = append(r, s.Address.StateSenateDistrict)
+	defer g.Close()
+	r := csv.NewReader(g)
+	rows, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Read %d rows from %s\n", len(rows), *csvFile)
+	//Clip headers.
+	rows = rows[1:len(rows)]
+
+	//Do in legal sized batches.  Each batch is a list of
+	//string slices.  Dereference those to get a list of emails.
+	for i := 0; i < len(rows); i += int(e.Metrics.MaxBatchSize) {
+		var emails []string
+		batch := rows[i : i+int(e.Metrics.MaxBatchSize)]
+		log.Printf("Batch at offset %d contains %d emails.", i, len(batch))
+		for _, v := range batch {
+			if len(v) > 0 {
+				emails = append(emails, v[0])
 			}
 		}
-		w.Write(r)
+
+		payload := goengage.SupporterSearchPayload{
+			Identifiers:    emails,
+			IdentifierType: goengage.EmailAddressType,
+			Offset:         0,
+			Count:          e.Metrics.MaxBatchSize,
+		}
+		rqt := goengage.SupporterSearch{
+			Header:  goengage.RequestHeader{},
+			Payload: payload,
+		}
+		var resp goengage.SupporterSearchResults
+
+		n := goengage.NetOp{
+			Host:     e.Host,
+			Method:   goengage.SearchMethod,
+			Endpoint: goengage.SearchSupporter,
+			Token:    e.Token,
+			Request:  &rqt,
+			Response: &resp,
+		}
+		err = n.Do()
+		if err != nil {
+			panic(err)
+		}
+		for _, s := range resp.Payload.Supporters {
+			email := goengage.FirstEmail(s)
+
+			if s.Result != goengage.Found {
+				fmt.Printf("%v %v %v %v\n",
+					s.FirstName,
+					s.LastName,
+					email,
+					s.Result)
+				continue
+			}
+			var a []string
+			e := ""
+			if email != nil {
+				e = *email
+			}
+			for i := 0; i < len(headers); i++ {
+				switch i {
+				case 0:
+					a = append(a, s.FirstName)
+				case 1:
+					a = append(a, s.LastName)
+				case 2:
+					a = append(a, e)
+				case 3:
+					a = append(a, s.Result)
+				case 4:
+					a = append(a, s.Address.AddressLine1)
+				case 5:
+					a = append(a, s.Address.AddressLine2)
+				case 6:
+					a = append(a, s.Address.City)
+				case 7:
+					a = append(a, s.Address.State)
+				case 8:
+					a = append(a, s.Address.PostalCode)
+				case 9:
+					a = append(a, s.Address.FederalHouseDistrict)
+				case 10:
+					a = append(a, s.Address.StateHouseDistrict)
+				case 11:
+					a = append(a, s.Address.StateSenateDistrict)
+				}
+			}
+			w.Write(a)
+		}
 	}
-	rqt.Payload.Offset += rqt.Payload.Count
 	w.Flush()
 	fmt.Printf("Done.  Output is in %v\n", fn)
 }
