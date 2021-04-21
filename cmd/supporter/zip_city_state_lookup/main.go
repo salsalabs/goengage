496c400b56e8b0d58af18a7b53b4082a6113f710
Allen Leonard <aleonard@salsalabs.com>
2021-04-21 16:04:26 -0500

    Working version.  Speeding up with channels.

--- cmd/supporter/zip_city_state_lookup/main.go
+++ cmd/supporter/zip_city_state_lookup/main.go
@@ -2,35 +2,101 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 
 	goengage "github.com/salsalabs/goengage/pkg"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
+
+//Zippopatamus is the record that's returned for a postalcode lookup.
+type Zippopatamus struct {
+	PostCode            string `json:"post code"`
+	Country             string `json:"country"`
+	CountryAbbreviation string `json:"country abbreviation"`
+	Places              []struct {
+		PlaceName         string `json:"place name"`
+		Longitude         string `json:"longitude"`
+		State             string `json:"state"`
+		StateAbbreviation string `json:"state abbreviation"`
+		Latitude          string `json:"latitude"`
+	} `json:"places"`
+}
+
+//Fix an address record using a call to Zippopatm.us.  Returns the modified
+//address record.  Errors trigger panics.
+func fixWithZip(a *goengage.Address) (*goengage.Address, error) {
+	url := fmt.Sprintf("https://api.zippopotam.us/us/%s", a.PostalCode)
+	// log.Printf("fixWithZip url: '%s'", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	// log.Printf("fixWithZip body: '%s'", string(body))
+	if err != nil {
+		return a, err
+	}
+	// Can get back an empty struct ("{}") here...
+	if len(body) > 2 {
+		var z Zippopatamus
+		err = json.Unmarshal(body, &z)
+		if err != nil {
+			return a, err
+		}
+		// log.Printf("fixWithZip z: %v\n", z)
+		a.City = z.Places[0].PlaceName
+		a.State = z.Places[0].StateAbbreviation
+	}
+	return a, nil
+}
 
 //Process one supporter record by fixing city and/or state using a lookup from
 //zippopatm.us.  Outputs a CSV row if either the city or state changes. Errors
 //trigger panics.
 func process(s goengage.Supporter, w *csv.Writer) {
 	email := goengage.FirstEmail(s)
-	lastModified := fmt.Sprintf("%v", s.LastModified)
 	e := ""
 	if email != nil {
 		e = *email
 	}
-	record := []string{
-		s.SupporterID,
-		s.FirstName,
-		s.LastName,
-		lastModified,
-		e,
+	a := s.Address
+	if a == nil {
+		a = &goengage.Address{
+			City:       "",
+			State:      "",
+			PostalCode: "",
+		}
 	}
-	err := w.Write(record)
-	if err != nil {
-		panic(err)
+	if len(a.PostalCode) != 0 && (len(a.City) == 0 || len(a.State) == 0) {
+		c := a.City
+		st := a.State
+		fixWithZip(a)
+		cityModified := a.City != c
+		stateModified := a.State != st
+		if cityModified || stateModified {
+			record := []string{
+				s.SupporterID,
+				e,
+				a.City,
+				a.State,
+				a.PostalCode,
+				fmt.Sprintf("%v", cityModified),
+				fmt.Sprintf("%v", stateModified),
+			}
+			err := w.Write(record)
+			log.Printf("%v\n", record)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
-
 }
 
 //Program entry point.  Look for supporters in a last_modified range.
@@ -39,8 +105,9 @@ func main() {
 	var (
 		app       = kingpin.New("ZIP City State Lookup", "Use Zippotam.us to find missing states and cities by postalCode")
 		login     = app.Flag("login", "YAML file with API token").Required().String()
-		startDate = app.Flag("start", "Start of the date range").Default("2001-01-01T00:00:00.000Z").String()
-		endDate   = app.Flag("end", "End of the date range").Default("2101-01-01T00:00:00.000Z").String()
+		startDate = app.Flag("start", "Last modified start").Default("2001-01-01T00:00:00.000Z").String()
+		endDate   = app.Flag("end", "Last modified end").Default("2101-01-01T00:00:00.000Z").String()
+		csvFile   = app.Flag("csv", "CSV to receive modified records").Default("zip_city_state_fixes.csv").String()
 	)
 	app.Parse(os.Args[1:])
 	if login == nil || len(*login) == 0 {
@@ -52,11 +119,13 @@ func main() {
 		panic(err)
 	}
 
-	f, err := os.Create("last_modified.csv")
+	f, err := os.Create(*csvFile)
 	if err != nil {
 		panic(err)
 	}
 	w := csv.NewWriter(f)
+	h := strings.Split("SupporterID,Email,City,State,PostalCode,CityModified,StateModifled", ",")
+	w.Write(h)
 
 	count := int32(e.Metrics.MaxBatchSize)
 	offset := int32(0)
@@ -81,6 +150,9 @@ func main() {
 			Request:  &rqt,
 			Response: &resp,
 		}
+		if offset%1000 == int32(0) {
+			log.Printf("main: %5d of %5d supporters\n", offset, resp.Payload.Total)
+		}
 		err = n.Do()
 		if err != nil {
 			panic(err)
@@ -90,5 +162,6 @@ func main() {
 			process(s, w)
 		}
 		offset += count
+		log.Printf("main: %d\n", offset)
 	}
 }
