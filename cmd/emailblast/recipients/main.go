dd465d0579be7224f306f3e6a15c94810f3d626e
Allen Leonard <aleonard@salsalabs.com>
2021-07-08 12:39:45 -0500

    Blast iteration done.  Working on individual blasts.

--- cmd/emailblast/recipients/main.go
+++ cmd/emailblast/recipients/main.go
@@ -1,77 +1,181 @@
 package main
 
-// An application to read all email blasts and write blast and
-//write blast and recipient activity to CSVs.  This should give
-//an Engage client a way to create history in a data warehouse.
+// Quick and dirty application to read all email blasts and write blast
+// and recipient activity to CSVs.  Need this done today.  No performance
+// tricks -- just getting the job done.
 import (
+	"encoding/csv"
+	"log"
+	"os"
+
 	goengage "github.com/salsalabs/goengage/pkg"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-//RecipientRecord contains both blast and recipient info.
-type RecipientRecord struct {
-	Recipient goengage.SingleBlastRecipient
-	Blast     goengage.EmailActivity
-}
-
-//Runtime contains the configuration parts that this app needs.
+//Runtime is the internal data store for this app.
 type Runtime struct {
-	BlastChan       chan goengage.EmailActivity
-	BlastCSVChan    chan goengage.EmailActivity
-	ConversionChan  chan goengage.Conversion
-	RecipientChan   chan RecipientRecord
-	DoneChan        chan bool
-	BlastOffset     int32
-	RecipientOffset int32
-	BlastCursor     *string
+	Env             *goengage.Environment
+	PublishedFrom   string
+	RecipientsFile  *csv.Writer
+	ConversionsFile *csv.Writer
 }
 
-//Bongos are drums.
-type Bongos struct {
-	Count int32
-}
+//Blasts reads email blasts and passes blasts off to the
+//blast detail reader.
+func (rt *Runtime) Blasts() error {
+	count := rt.Env.Metrics.MaxBatchSize
+	offset := int32(0)
+	for count == rt.Env.Metrics.MaxBatchSize {
+		payload := goengage.EmailBlastSearchRequestPayload{
+			PublishedFrom: rt.PublishedFrom,
+			Offset:        offset,
+			Count:         count,
+			Type:          goengage.Email,
+		}
+		rqt := goengage.EmailBlastSearchRequest{
+			Header:  goengage.RequestHeader{},
+			Payload: payload,
+		}
+		var resp goengage.EmailBlastSearchResponse
 
-//These declarations are used by the goengage blast report
-//handler (goengage.ProcessEmailBlasts) to process blasts
-//and history.
-
-//Visit some bongos.
-func (b *Bongos) Visit() {
-
-}
-
-//Visit does something with the blast. Errors terminate.
-//Implements goengage.EmailBlastGuide.
-func (rt *Runtime) Visit(s goengage.EmailActivity) error {
-	rt.BlastCSVChan <- s
+		n := goengage.NetOp{
+			Host:     rt.Env.Host,
+			Method:   goengage.SearchMethod,
+			Endpoint: goengage.EmailBlastSearch,
+			Token:    rt.Env.Token,
+			Request:  &rqt,
+			Response: &resp,
+		}
+		err := n.Do()
+		if err != nil {
+			return err
+		}
+		for _, s := range resp.Payload.EmailActivities {
+			err = rt.OneBlast(s)
+			if err != nil {
+				return err
+			}
+		}
+		offset = offset + count
+		count = resp.Payload.Count
+	}
 	return nil
 }
 
-//Finalize is called after all blasts have been processed.
-//Implements goengage.EmailBlastGuide.
-func (rt *Runtime) Finalize() error {
+//Details reads the activity version of the blast and writes recipients
+//and conversions.
+func (rt *Runtime) OneBlast(r goengage.EmailActivity) error {
+	log.Printf("OneBlast: %s %s\n", r.ID, r.Name)
+	count := rt.Env.Metrics.MaxBatchSize
+	offset := int32(0)
+	cursor := ""
+	// for count == rt.Env.Metrics.MaxBatchSize {
+	log.Printf("OneBlast: ID: %s, cursor: %s, offset: %d, count: %d\n", id, cursor, offset, count)
+	payload := goengage.IndivualBlastRequestPayload{
+		ID:     id,
+		Offset: offset,
+		Count:  count,
+		Type:   goengage.Email,
+	}
+	if len(cursor) > 0 {
+		payload.Cursor = cursor
+	}
+
+	rqt := goengage.IndivualBlastRequest{
+		Header:  goengage.RequestHeader{},
+		Payload: payload,
+	}
+	var resp goengage.InvidualBlastResponse
+
+	n := goengage.NetOp{
+		Host:     rt.Env.Host,
+		Method:   goengage.SearchMethod,
+		Endpoint: goengage.IndividualBlastSearch,
+		Token:    rt.Env.Token,
+		Request:  &rqt,
+		Response: &resp,
+	}
+	err := n.Do()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("OneBlast: response %+v\n", resp)
+	for _, s := range resp.Payload.IndividualEmailActivityData {
+		log.Printf("OneBlast: individual activity %+v\n", s)
+		err = rt.Recipients(id, s.RecipientsData)
+		if err != nil {
+			return err
+		}
+		// cursor = s.Cursor
+		// if len(cursor) == 0 {
+		// 	log.Printf("OneBlast: cursor is empty\n")
+		// 	count = 0
+		// } else {
+		// count = int32(len(s.RecipientsData.Recipients))
+		// }
+		offset = offset + count
+	}
+	offset = offset + count
+	// }
 	return nil
 }
 
-//Payload is the request payload defining which supporters to retrieve.
-//Implements goengage.EmailBlastGuide.
-func (rt *Runtime) Payload() goengage.EmailBlastSearchRequestPayload {
-	payload := goengage.EmailBlastSearchRequestPayload{}
-	return payload
+//Recipients formats and writes email and conversion activity.
+func (rt *Runtime) Recipients(blastId string, r goengage.SingleBlastRecipientsData) error {
+	log.Printf("Recipients: blast ID: %s, %d recipients", blastId, len(r.Recipients))
+	return nil
 }
 
-//Channel is the listener channel to use.
-func (rt *Runtime) Channel() chan goengage.EmailActivity {
-	return rt.BlastChan
-}
+//Program entry point.
+func main() {
+	var (
+		app             = kingpin.New("recipients", "Write recipient and conversion data for blasts sent after a date")
+		login           = app.Flag("login", "YAML file with API token").Required().String()
+		recipientsFile  = app.Flag("recipients", "CSV filename to recipient info").Default("recipients.csv").String()
+		conversionsFile = app.Flag("conversions", "CSV filename to store conversion info").Default("conversion.csv").String()
+		publishedFrom   = app.Flag("published-from", "Engage-formatted start date").Default("2021-03-12T00:00:00.000Z").String()
+	)
+	app.Parse(os.Args[1:])
+	if login == nil || len(*login) == 0 {
+		log.Fatalf("Error --login is required.")
+		os.Exit(1)
+	}
+	if recipientsFile == nil || len(*recipientsFile) == 0 {
+		log.Fatalf("Error --blast-csv is required.")
+		os.Exit(1)
+	}
+	if conversionsFile == nil || len(*conversionsFile) == 0 {
+		log.Fatalf("Error --csv is required.")
+		os.Exit(1)
+	}
+	e, err := goengage.Credentials(*login)
+	if err != nil {
+		log.Fatalf("Error %v\n", err)
+		os.Exit(1)
+	}
+	f1, err := os.Create(*recipientsFile)
+	if err != nil {
+		log.Fatalf("Error %v\n", err)
+		os.Exit(1)
+	}
+	defer f1.Close()
 
-//DoneChannel receives a true when the listener is done.
-//Implements goengage.EmailBlastGuide.
-func (rt *Runtime) DoneChannel() chan bool {
-	return rt.DoneChan
-}
+	f2, err := os.Create(*conversionsFile)
+	if err != nil {
+		log.Fatalf("Error %v\n", err)
+		os.Exit(1)
+	}
+	defer f2.Close()
 
-//Offset returns the offset to start reading.
-//Implements goengage.EmailBlastGuide.
-func (rt *Runtime) Offset() int32 {
-	return rt.BlastOffset
+	rtx := Runtime{
+		Env:             e,
+		PublishedFrom:   *publishedFrom,
+		RecipientsFile:  csv.NewWriter(f1),
+		ConversionsFile: csv.NewWriter(f2),
+	}
+	rt := &rtx
+
+	rt.Blasts()
+
 }
