ab5528c7adbdbbedf3c2972fa7979c086fab0ea3
Allen Leonard <aleonard@salsalabs.com>
2021-09-01 17:17:55 -0500

    New app. CSV of phone numbers.  Works.

--- cmd/supporter/phone_numbers/main.go
+++ cmd/supporter/phone_numbers/main.go
@@ -2,10 +2,8 @@
 //data is stored in a CSV.  The CSV has one row per supporter. Each row
 //contains SupporterID, Home Phone, Cell Phone, and Work Phone.
 //
-//Unlike Classic, phone numbers are not fixed fields. They are elements
-//in the "Contacts" part of the supporter record.  Note that Engage does
-//not send output for fields that do not have values. You'll see a lot of
-//"use this field if it exists or substitute an empty string" in this app.
+//Unlike Classic, phone numbers in Engage are not fixed fields. They are
+//elements in the "Contacts" part of the supporter record.
 package main
 
 import (
@@ -13,6 +11,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strings"
 	"sync"
@@ -26,20 +25,22 @@ import (
 //Runtime area for this app.
 type Runtime struct {
 	E         *goengage.Environment
-	ChunkChan chan []string
 	WriteChan chan goengage.Supporter
 	DoneChan  chan bool
 	IDFile    string
+	IDs       []string
+	IdOffset  int32
 	CSVOut    *csv.Writer
 }
 
 //RequestedIDs returns the list of supporterIDs from the ID file.
 //Each line of the file is a single Supporter ID.
-func (rt *Runtime) RequestedIds() (a []string, err error) {
+func (rt *Runtime) RequestedIds() error {
 	r, err := os.Open(rt.IDFile)
 	if err != nil {
-		return a, err
+		return err
 	}
+	var a []string
 	defer r.Close()
 	fs := bufio.NewScanner(r)
 	fs.Split(bufio.ScanLines)
@@ -47,21 +48,23 @@ func (rt *Runtime) RequestedIds() (a []string, err error) {
 		id := fs.Text()
 		id = strings.Trim(id, "'\" \t")
 		if len(id) != 36 {
-			log.Fatalf("RequestedIds: file %v, '%v' is not a valid id\n", rt.IDFile, id)
+			err = fmt.Errorf("not a vaid supporterID, file %v, entry '%v'", rt.IDFile, id)
+			return err
 		}
 		a = append(a, id)
 	}
-	return a, err
+	rt.IDs = a
+	return nil
 }
 
 //NewRuntime populates a new runtime.
-func NewRuntime(env *goengage.Environment, idFile string, out *csvWriter) Runtime {
+func NewRuntime(env *goengage.Environment, idFile string, out *csv.Writer) Runtime {
 	r := Runtime{
 		E:         env,
-		ChunkChan: make(chan []string, 100),
 		WriteChan: make(chan goengage.Supporter, 100),
 		DoneChan:  make(chan bool),
 		IDFile:    idFile,
+		IdOffset:  0,
 		CSVOut:    out,
 	}
 	return r
@@ -73,7 +76,10 @@ func (r *Runtime) Visit(s goengage.Supporter) error {
 	if s.Contacts == nil {
 		return nil
 	}
-	row := make([]string, 4)
+	var row []string
+	for i := 0; i < 4; i++ {
+		row = append(row, "")
+	}
 	row[0] = s.SupporterID
 	for _, c := range s.Contacts {
 		switch c.Type {
@@ -81,14 +87,15 @@ func (r *Runtime) Visit(s goengage.Supporter) error {
 			row[1] = c.Value
 		case goengage.ContactCell:
 			row[2] = c.Value
-		case goEngage.ContactWork:
+		case goengage.ContactWork:
 			row[3] = c.Value
 		}
 	}
-	err := r.CSVOut.write(row)
+	err := r.CSVOut.Write(row)
 	if err != nil {
 		return err
 	}
+	r.CSVOut.Flush()
 	log.Println(row)
 	return nil
 }
@@ -96,25 +103,31 @@ func (r *Runtime) Visit(s goengage.Supporter) error {
 //Finalize implements SupporterGuide.Filnalize and does nothing
 //in this app.
 func (r *Runtime) Finalize() error {
+	return nil
 }
 
 //Payload implements SupporterGuide.Payload and provides a payload
 //that will retrieve all supporters.
 func (r *Runtime) Payload() goengage.SupporterSearchRequestPayload {
+	low := float64(r.IdOffset)
+	remaining := float64(len(r.IDs)) - float64(low)
+	high := low + math.Min(remaining, float64(r.E.Metrics.MaxBatchSize))
+	max := len(r.IDs)
+	current := r.IDs[int32(low):int32(high):max]
 	payload := goengage.SupporterSearchRequestPayload{
 		IdentifierType: goengage.SupporterIDType,
-		ModifiedFrom:   "2000-01-01T00:00:00.00000Z",
-		ModifiedTo:     "2050-01-01T00:00:00.00000Z",
+		Identifiers:    current,
 		Offset:         0,
 		Count:          0,
 	}
+	r.IdOffset += r.E.Metrics.MaxBatchSize
 	return payload
 }
 
 //Channel implements SupporterGuide.Channnel and provides the
 //supporter channel.
 func (r *Runtime) Channel() chan goengage.Supporter {
-	return r.InChan
+	return r.WriteChan
 }
 
 //DoneChannel implements SupporterGuide.DoneChannel to provide
@@ -126,17 +139,30 @@ func (r *Runtime) DoneChannel() chan bool {
 //Offset returns the offset for the first read.
 //Useful for restarts.
 func (r *Runtime) Offset() int32 {
-	return r.ReadOffset
+	return 0
+}
+
+//AdjustOffset allows us to change the read offset to
+//zero for the payload that we're using.
+
+func (r *Runtime) AdjustOffset(offset int32) int32 {
+	if r.IdOffset > int32(len(r.IDs)) {
+		// Offset that wll hopefully trigger end of file.
+		// Yeah, this needs some work...
+		return 1_000_000_000
+	} else {
+		return 0
+	}
 }
 
 //Program entry point.  Look for supporters with an email.  Errors are noisy and fatal.
 func main() {
 	var (
-		app     = kingpin.New("segments_for_supporters", "Write a CSV of supporters and segments for a list of supporter IDs")
+		app     = kingpin.New("phone_numbers", "Write a CSV of supporterIDs and phone numbers")
 		login   = app.Flag("login", "YAML file with API token").Required().String()
 		idFile  = app.Flag("input", "Text with list of Engage supporterIDs to look up").Required().String()
-		outFile = app.Flag("output", "CSV filename to store supporter-segment data").Default("supporters_and_segments.csv").String()
-		debug   = app.Flag("debug", "Write requests and responses to a log file in JSON").Bool()
+		outFile = app.Flag("output", "CSV filename to store supporter-segment data").Default("phone_numbers.csv").String()
+		//debug   = app.Flag("debug", "Write requests and responses to a log file in JSON").Bool()
 	)
 	app.Parse(os.Args[1:])
 	if login == nil || len(*login) == 0 {
@@ -148,7 +174,7 @@ func main() {
 		panic(err)
 	}
 
-	f, err := os.Create(csvFile)
+	f, err := os.Create(*outFile)
 	if err != nil {
 		panic(err)
 	}
@@ -160,28 +186,37 @@ func main() {
 		"CellPhone",
 		"WorkPhone",
 	}
+	err = writer.Write(headers)
+	if err != nil {
+		panic(err)
+	}
 
-	r := NewRuntime(e, *fieldName)
+	r := NewRuntime(e, *idFile, writer)
+	err = r.RequestedIds()
+	if err != nil {
+		panic(err)
+	}
+
 	var wg sync.WaitGroup
 
 	//Start supporter listener. Only one of these because Visit is quick
 	//in this app. More than one cases "concurrent map writes" errors.
+	wg.Add(1)
 	go (func(e *goengage.Environment, r *Runtime, wg *sync.WaitGroup) {
-		wg.Add(1)
 		defer wg.Done()
 		reportSupporter.ProcessSupporters(r.E, r)
 	})(e, &r, &wg)
 
 	//Start done listener.
+	wg.Add(1)
 	go (func(r *Runtime, n int, wg *sync.WaitGroup) {
-		wg.Add(1)
 		defer wg.Done()
 		goengage.DoneListener(r.DoneChan, n)
 	})(&r, 1, &wg)
 
 	//Start supporter reader.
+	wg.Add(1)
 	go (func(e *goengage.Environment, r *Runtime, wg *sync.WaitGroup) {
-		wg.Add(1)
 		defer wg.Done()
 		reportSupporter.ReadSupporters(r.E, r)
 	})(e, &r, &wg)
