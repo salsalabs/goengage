e8d4e7b2c82e3a30ff5d9b90759f02de386cbca9
Allen Leonard <aleonard@salsalabs.com>
2021-12-23 16:32:02 -0600

    Added demos of transactions by supporter and activityFormID.

---
+++ cmd/transaction/activity_form/main.go
@@ -0,0 +1,161 @@
+package main
+
+//Application to create a CSV of transactions for a activity_form. You
+//provide credentials and a activity_form_KEY.  This app writes a CSV of
+//the activity_form's transactions.
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+
+	goengage "github.com/salsalabs/goengage/pkg"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+//Runtime holds the stuff that this app needs.
+type Runtime struct {
+	Env            *goengage.Environment
+	IncludeCounts  bool
+	CSVFilename    string
+	Logger         *goengage.UtilLogger
+	ActivityFormID string
+}
+
+func NewRuntime(e *goengage.Environment, f string, s string, v bool) (*Runtime, error) {
+	rt := Runtime{
+		Env:            e,
+		CSVFilename:    f,
+		ActivityFormID: s,
+	}
+	if v {
+		logger, err := goengage.NewUtilLogger()
+		if err != nil {
+			return nil, err
+		}
+		rt.Logger = logger
+	}
+	return &rt, nil
+}
+
+//Run finds and displays all transactions.
+func Run(rt *Runtime) error {
+	log.Println("Run: begin")
+	f, err := os.Create(rt.CSVFilename)
+	if err != nil {
+		return err
+	}
+	writer := csv.NewWriter(f)
+	headers := []string{
+		"CreatedDate",
+		"TransactionID",
+		"ActivityName",
+		"ActivityFormID",
+		"Amount",
+		"TemplateID",
+		"TransactionDate",
+		"TransactionType",
+		"DeductibleAmount",
+		"FeesPaid",
+		"Result",
+	}
+	err = writer.Write(headers)
+	if err != nil {
+		return err
+	}
+
+	count := rt.Env.Metrics.MaxBatchSize
+	offset := int32(0)
+
+	for count == rt.Env.Metrics.MaxBatchSize {
+		payload := goengage.TransactionSearchRequestPayload{
+			Identifiers:    []string{rt.ActivityFormID},
+			IdentifierType: goengage.ActivityFormID,
+			Count:          count,
+			Offset:         offset,
+		}
+		fmt.Printf("Request payload: %+v\n", payload)
+		rqt := goengage.TransactionSearchRequest{
+			Header:  goengage.RequestHeader{},
+			Payload: payload,
+		}
+
+		var resp goengage.TransactionSearchResponse
+
+		n := goengage.NetOp{
+			Host:     rt.Env.Host,
+			Method:   goengage.SearchMethod,
+			Endpoint: goengage.SearchTransactionDetails,
+			Token:    rt.Env.Token,
+			Request:  &rqt,
+			Response: &resp,
+			Logger:   rt.Logger,
+		}
+		err := n.Do()
+		if err != nil {
+			return err
+		}
+		if offset%100 == 0 {
+			log.Printf("Run: %6d: %2d of %6d\n",
+				offset,
+				len(resp.Payload.Transactions),
+				resp.Payload.Total)
+		}
+
+		var cache [][]string
+		for _, w := range resp.Payload.Transactions {
+			s := w.DonationTransaction
+			record := []string{
+				s.CreatedDate,
+				s.TransactionID,
+				s.ActivityName,
+				s.ActivityFormID,
+				fmt.Sprintf("%v", s.Amount),
+				s.TemplateID,
+				s.TransactionDate,
+				s.TransactionType,
+				fmt.Sprintf("%v", s.DeductibleAmount),
+				fmt.Sprintf("%v", s.FeesPaid),
+				s.Result,
+			}
+			cache = append(cache, record)
+		}
+		err = writer.WriteAll(cache)
+		if err != nil {
+			return err
+		}
+		count = resp.Payload.Count
+		offset += int32(count)
+	}
+	log.Printf("Run: end")
+	return nil
+}
+
+//Program entry point.
+func main() {
+	var (
+		app            = kingpin.New("activity_form_transactions", "Creates a CSV of transactions for a activity_form")
+		login          = app.Flag("login", "YAML file with API token").Required().String()
+		csvFile        = app.Flag("csv", "CSV filename to create").Default("activity_form_transactions.csv").String()
+		activityFormID = app.Flag("activityFormID", "Find transactions for this activity_form").Required().String()
+		verbose        = app.Flag("verbose", "Log all requests and responses to a file.  Verrrry noisy...").Bool()
+	)
+	app.Parse(os.Args[1:])
+	if login == nil || len(*login) == 0 {
+		log.Fatalf("Error --login is required.")
+		os.Exit(1)
+	}
+	e, err := goengage.Credentials(*login)
+	if err != nil {
+		log.Fatalf("main: %+v\n", e)
+	}
+	rt, err := NewRuntime(e, *csvFile, *activityFormID, *verbose)
+	if err != nil {
+		log.Fatalf("main: %v\n", err)
+	}
+	err = Run(rt)
+	if err != nil {
+		log.Fatalf("main: %v\n", err)
+	}
+}
--- pkg/transaction.go
+++ pkg/transaction.go
@@ -11,52 +11,33 @@ const (
 //Transaction is an Engage donation template. Part of transactions in
 //the API documentation.
 type DonationTransaction struct {
-	CreatedBy              string `json:"createdBy,omitempty"`
-	CreatedDate            string `json:"createdDate,omitempty"`
-	ModifiedBy             string `json:"modifiedBy,omitempty"`
-	LastModified           string `json:"lastModified,omitempty"`
-	TransactionID          string `json:"templateId,omitempty"`
-	ActivityDate           string `json:"activityDate,omitempty"`
-	PersonID               string `json:"personId,omitempty"`
-	ActivityID             string `json:"activityId,omitempty"`
-	ActivityFormID         string `json:"activityFormId,omitempty"`
-	ActivityName           string `json:"activityName,omitempty"`
-	AccountExpiration      string `json:"accountExpiration,omitempty"`
-	AccountType            string `json:"accountType,omitempty"`
-	DonationType           string `json:"donationType,omitempty"`
-	LastTransactionType    string `json:"lastTransactionType,omitempty"`
-	OneTimeAmount          int    `json:"oneTimeAmount,omitempty"`
-	RecurringAmount        int    `json:"recurringAmount,omitempty"`
-	TotalReceivedAmount    int    `json:"totalReceivedAmount,omitempty"`
-	RecurringEnd           string `json:"recurringEnd,omitempty"`
-	RecurringInterval      string `json:"recurringInterval,omitempty"`
-	RecurringStart         string `json:"recurringStart,omitempty"`
-	RecurringTransactionID string `json:"recurringTransactionId,omitempty"`
-	IsFirstDonation        bool   `json:"isFirstDonation,omitempty"`
-	Dedication             string `json:"dedication,omitempty"`
-	DedicationType         string `json:"dedicationType,omitempty"`
-	Designation            string `json:"designation,omitempty"`
-	Notify                 string `json:"notify,omitempty"`
-	WasImported            bool   `json:"wasImported,omitempty"`
-	ReceivedAmountDonation int    `json:"receivedAmountDonation,omitempty"`
-	FeesPaid               int    `json:"feesPaid,omitempty"`
-	ReceivedAmountTickets  int    `json:"receivedAmountTickets,omitempty"`
-	Appeal                 string `json:"appeal,omitempty"`
-	Campaign               string `json:"campaign,omitempty"`
-	AppealName             string `json:"appealName,omitempty"`
-	CampaignName           string `json:"campaignName,omitempty"`
-	Fund                   string `json:"fund,omitempty"`
-	FundName               string `json:"fundName,omitempty"`
-	ReceivedAmountProducts int    `json:"receivedAmountProducts,omitempty"`
-	IsAnonymous            bool   `json:"isAnonymous,omitempty"`
-	DisplayName            string `json:"displayName,omitempty"`
-	WasAPIImported         bool   `json:"wasApiImported,omitempty"`
-	ExternalID             string `json:"externalId,omitempty"`
-	DoNotSyncCrm           bool   `json:"doNotSyncCrm,omitempty"`
-	HideAmount             bool   `json:"hideAmount,omitempty"`
-	SmartAmount            bool   `json:"smartAmount,omitempty"`
-	OpenEnded              bool   `json:"openEnded,omitempty"`
-	Result                 string `json:"result,omitempty"`
+	CreatedBy              string  `json:"createdBy,omitempty"`
+	CreatedDate            string  `json:"createdDate,omitempty"`
+	ModifiedBy             string  `json:"modifiedBy,omitempty"`
+	LastModified           string  `json:"lastModified,omitempty"`
+	TransactionID          string  `json:"transactionId,omitempty"`
+	ActivityID             string  `json:"activityId,omitempty"`
+	ActivityFormID         string  `json:"activityFormId,omitempty"`
+	ActivityName           string  `json:"activityName,omitempty"`
+	SupporterID            string  `json:"supporterId,omitempty"`
+	AccountExpiration      string  `json:"accountExpiration,omitempty"`
+	AccountType            string  `json:"accountType,omitempty"`
+	Amount                 float32 `json:"amount,omitempty"`
+	TemplateID             string  `json:"templateId,omitempty"`
+	RelatedTransactionID   string  `json:"relatedTransactionId,omitempty"`
+	TransactionDate        string  `json:"transactionDate,omitempty"`
+	Transaction            string  `json:"transaction,omitempty"`
+	TransactionType        string  `json:"transactionType,omitempty"`
+	ConfirmationEmailID    string  `json:"confirmationEmailId,omitempty"`
+	ConfirmationEmailSent  string  `json:"confirmationEmailSent,omitempty"`
+	WasImported            bool    `json:"wasImported,omitempty"`
+	WasOffline             bool    `json:"wasOffline,omitempty"`
+	DeductibleAmount       float32 `json:"deductibleAmount,omitempty"`
+	FeesPaid               float32 `json:"feesPaid,omitempty"`
+	WasAPIImported         bool    `json:"wasApiImported,omitempty"`
+	ConvertedValidityCheck bool    `json:"convertedValidityCheck,omitempty"`
+	ValidityCheckDate      string  `json:"validityCheckDate,omitempty"`
+	Result                 string  `json:"result,omitempty"`
 }
 
 //TransactionSearchRequest contains parameters for searching for segments.
@@ -73,8 +54,8 @@ type TransactionSearchRequestPayload struct {
 	IdentifierType  string   `json:"identifierType,omitEmpty"`
 	TransactionFrom string   `json:"transactionFrom,omitEmpty"`
 	TransactionTo   string   `json:"transactionTo,omitEmpty"`
-	Offset          int      `json:"offset,omitEmpty"`
-	Count           int      `json:"count,omitEmpty"`
+	Offset          int32    `json:"offset,omitEmpty"`
+	Count           int32    `json:"count,omitEmpty"`
 }
 
 //TransactionSearchResponse contains the results returned by searching for segments.
@@ -99,5 +80,5 @@ type TransactionSearchResponsePayload struct {
 	Count        int32                `json:"count,omitempty"`
 	Offset       int32                `json:"offset,omitempty"`
 	Total        int32                `json:"total,omitempty"`
-	Transactions []TransactionWrapper `json:"transactionTransactions,omitempty"`
+	Transactions []TransactionWrapper `json:"transactions,omitempty"`
 }
