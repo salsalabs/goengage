b15fb319bf9259d131b49180ccc51090c9765543
Allen Leonard <aleonard@salsalabs.com>
2022-10-06 15:11:16 -0500

    Add missing fields, formatting.

--- pkg/activity.go
+++ pkg/activity.go
@@ -2,17 +2,17 @@ package goengage
 
 import "time"
 
-//ActivityRequest is used to retrieve activities from Engage.
-//Note that ActivityRequest can be used to retrieve activities based
-//on three types of criteria: activity IDs, activity form IDs, modified
-//date range.  Choose one and provide the necessary data.  The remainder
-//will be ignored when the request is sent to Engage.
+// ActivityRequest is used to retrieve activities from Engage.
+// Note that ActivityRequest can be used to retrieve activities based
+// on three types of criteria: activity IDs, activity form IDs, modified
+// date range.  Choose one and provide the necessary data.  The remainder
+// will be ignored when the request is sent to Engage.
 type ActivityRequest struct {
 	Header  RequestHeader          `json:"header"`
 	Payload ActivityRequestPayload `json:"payload"`
 }
 
-//ActivityRequestPayload specifies the activities to return.
+// ActivityRequestPayload specifies the activities to return.
 type ActivityRequestPayload struct {
 	Type            string   `json:"type,omitempty"`
 	Offset          int32    `json:"offset"`
@@ -21,11 +21,13 @@ type ActivityRequestPayload struct {
 	ActivityFormIDs []string `json:"activityFormIds,omitempty"`
 	ModifiedFrom    string   `json:"modifiedFrom,omitempty"`
 	ModifiedTo      string   `json:"modifiedTo,omitempty"`
+	SupporterIDs    []string `json:"supporterIds,omitempty"`
+	IdentifierType  string   `json:"identifierType,omitempty"`
 }
 
-//BaseActivity returns activity information from SUBSCRIBE or
-//SUBSCRIPTION_MANAGEMENT requests.  Note that Base is actually
-//contained in the other activity result objects.
+// BaseActivity returns activity information from SUBSCRIBE or
+// SUBSCRIPTION_MANAGEMENT requests.  Note that Base is actually
+// contained in the other activity result objects.
 type BaseActivity struct {
 	ActivityType      string             `json:"activityType,omitempty"`
 	ActivityID        string             `json:"activityId,omitempty"`
@@ -41,16 +43,23 @@ type BaseActivity struct {
 	TrackingCode      string             `json:"trackingCode,omitempty"`
 }
 
-//BaseResponse is the set of common fields returned for all activities.
-//Some activities (like SUBSCRIBE or SUBSCRIPTION_MANAGEMENT) only return
-//ActivityBase.  Other activities, like donations, events and P2P, return
-//data appended to the base.
+// BasePayload is returned by calls to fetch SUBSCRIBE or
+// SUBSCRIPTION_MANAGEMENT records.
+type BasePayload struct {
+	Total      int32          `json:"total,omitempty"`
+	Offset     int32          `json:"offset,omitempty"`
+	Count      int32          `json:"count,omitempty"`
+	Activities []BaseActivity `json:"activities,omitempty"`
+}
+
+// BaseResponse is the set of common fields returned for all activities.
+// Some activities (like SUBSCRIBE or SUBSCRIPTION_MANAGEMENT) only return
+// ActivityBase.  Other activities, like donations, events and P2P, return
+// data appended to the base.
 type BaseResponse struct {
-	Header  Header `json:"header,omitempty"`
-	Payload struct {
-		Total      int32          `json:"total,omitempty"`
-		Offset     int32          `json:"offset,omitempty"`
-		Count      int32          `json:"count,omitempty"`
-		Activities []BaseActivity `json:"activities,omitempty"`
-	} `json:"payload,omitempty"`
+	ID        string      `json:"id"`
+	Timestamp *time.Time  `json:"timestamp"`
+	Header    Header      `json:"header"`
+	Payload   BasePayload `json:"payload,omitempty"`
+	Errors    []Error     `json:"errors,omitempty"`
 }
